d1a6d8bd73f89e6bd236bd952a5850c1ed9dc614
Bamboo <13664854532@163.com>
2024-09-07 19:10:41 +0800

    update

--- internal/model/user.go
+++ internal/model/user.go
@@ -10,7 +10,7 @@ type User struct {
 	Password    string  `json:"password" gorm:"type:varchar(255);not null;comment:用户登录密码"`            // 用户登录密码，非空
 	RealName    string  `gorm:"type:varchar(100);comment:用户真实姓名"`                                     // 用户真实姓名
 	Desc        string  `gorm:"type:text;comment:用户描述"`                                               // 用户描述，支持较长文本
-	Mobile      string  `gorm:"type:varchar(20);uniqueIndex;comment:手机号"`                             // 手机号，唯一索引
+	Mobile      string  `json:"mobile" gorm:"type:varchar(20);uniqueIndex;comment:手机号"`               // 手机号，唯一索引
 	LarkUserId  string  `gorm:"type:varchar(50);comment:飞书用户ID"`                                      // 飞书用户ID
 	AccountType int     `gorm:"default:1;comment:账号类型 1普通用户 2服务账号"`                                   // 账号类型，默认为普通用户
 	HomePath    string  `gorm:"type:varchar(255);comment:登录后的默认首页"`                                   // 登录后的默认首页
