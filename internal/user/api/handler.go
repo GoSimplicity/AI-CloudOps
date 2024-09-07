72cdb6086270e8dce9635175b3de9cbdede369a6
shixiaocaia <shixiaocaia@gmail.com>
2024-09-07 16:04:56 +0800

    fix signup

--- internal/user/api/handler.go
+++ internal/user/api/handler.go
@@ -1,26 +1,32 @@
 package api
 
 import (
-	"net/http"
+	"errors"
 
+	. "github.com/GoSimplicity/CloudOps/pkg/ginp"
+
+	"github.com/GoSimplicity/CloudOps/internal/constants"
 	"github.com/GoSimplicity/CloudOps/internal/user/dto"
 	"github.com/GoSimplicity/CloudOps/internal/user/service"
 	"github.com/gin-gonic/gin"
+	"go.uber.org/zap"
 )
 
 type UserHandler struct {
 	service service.UserService
+	l       *zap.Logger
 }
 
-func NewUserHandler(service service.UserService) *UserHandler {
+func NewUserHandler(service service.UserService, l *zap.Logger) *UserHandler {
 	return &UserHandler{
 		service: service,
+		l:       l,
 	}
 }
 
 func (u *UserHandler) RegisterRoutes(server *gin.Engine) {
 	userGroup := server.Group("/api/users")
-	userGroup.POST("/signup", u.SignUp)
+	userGroup.POST("/signup", WrapBody(u.SignUp))
 	userGroup.POST("/login", u.Login)
 	userGroup.GET("/profile", u.Profile)
 
@@ -29,20 +35,24 @@ func (u *UserHandler) RegisterRoutes(server *gin.Engine) {
 	})
 }
 
-func (u *UserHandler) SignUp(ctx *gin.Context) {
-	var req dto.UserDTO
-
-	if err := ctx.ShouldBindJSON(&req); err != nil {
-		ctx.JSON(400, gin.H{"error": err.Error()})
-		return
-	}
-
-	res, err := u.service.SignUp(ctx, req)
+func (u *UserHandler) SignUp(ctx *gin.Context, req dto.UserDTO) (Result, error) {
+	err := u.service.SignUp(ctx, req)
 	if err != nil {
-		ctx.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"error": "服务器内部错误"})
+		if errors.Is(err, constants.ErrCodeDuplicateUserNameOrMobile) {
+			return Result{
+				Code: constants.UserExistErrorCode,
+				Msg:  constants.ErrorUserExist.Error(),
+			}, nil
+		}
+		return Result{
+			Code: constants.UserSignUpFailedErrorCode,
+			Msg:  constants.ErrorUserSignUpFail.Error(),
+		}, err
 	}
-
-	ctx.JSON(http.StatusOK, res)
+	return Result{
+		Code: constants.SuccessCode,
+		Msg:  constants.SuccessMsg,
+	}, nil
 }
 
 func (u *UserHandler) Login(ctx *gin.Context) {
