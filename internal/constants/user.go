0add5c7b4e1e7400b998e1ac5524687c2e092045
shixiaocaia <shixiaocaia@gmail.com>
2024-09-08 12:21:08 +0800

    err信息

--- internal/constants/user.go
+++ internal/constants/user.go
@@ -10,8 +10,8 @@ const (
 )
 
 var (
-	ErrorUserExist         = errors.New("user already exists, please check your username or mobile, or try to login")
+	ErrorUserExist         = errors.New("user already exists, check your username or mobile, or try to login")
 	ErrorUserNotExist      = errors.New("user not exists")
 	ErrorUserSignUpFail    = errors.New("user sign up fail")
-	ErrorPasswordIncorrect = errors.New("password incorrect")
+	ErrorPasswordIncorrect = errors.New("user password incorrect")
 )
