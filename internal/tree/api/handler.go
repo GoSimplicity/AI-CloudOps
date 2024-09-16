b4464b7a7c56abb3b48bbaa9653f1087d0e82747
shixiaocaia <shixiaocaia@gmail.com>
2024-09-16 10:01:56 +0800

    newtreeService

--- internal/tree/api/handler.go
+++ internal/tree/api/handler.go
@@ -1,8 +1,36 @@
 package api
 
-/**
- * @Author: Bamboo
- * @Author: 13664854532@163.com
- * @Date: 2024/9/15 16:30
- * @Desc:
- */
+import (
+	"github.com/GoSimplicity/CloudOps/internal/tree/service"
+	"github.com/gin-gonic/gin"
+	"go.uber.org/zap"
+)
+
+type TreeHandler struct {
+	service service.TreeService
+	l       *zap.Logger
+}
+
+func NewTreeHandler(service service.TreeService, l *zap.Logger) *TreeHandler {
+	return &TreeHandler{
+		service: service,
+		l:       l,
+	}
+}
+
+func (t *TreeHandler) RegisterRouters(server *gin.Engine) {
+	treeGroup := server.Group("/api/tree")
+	// Casbin 检查权限的中间件
+
+	treeGroup.POST("/resource/ecs", t.CreateResourceEcs)
+	// hello world
+	treeGroup.GET("/hello", func(ctx *gin.Context) {
+		ctx.JSON(200, gin.H{
+			"message": "hello world",
+		})
+	})
+}
+
+func (t *TreeHandler) CreateResourceEcs(ctx *gin.Context) {
+
+}
