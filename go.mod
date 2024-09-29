c0684ef19d59946cda13b833dcbdf49d60fbf34a
Bamboo <13664854532@163.com>
2024-09-29 10:46:01 +0800

    完成prometheus 配置缓存

--- go.mod
+++ go.mod
@@ -18,6 +18,8 @@ require (
 	github.com/google/wire v0.6.0
 	github.com/openkruise/kruise-api v1.7.0
 	github.com/prometheus/alertmanager v0.27.0
+	github.com/prometheus/common v0.55.0
+	github.com/prometheus/prometheus v0.54.1
 	github.com/redis/go-redis/v9 v9.6.1
 	github.com/robfig/cron/v3 v3.0.1
 	github.com/spf13/pflag v1.0.5
@@ -35,11 +37,17 @@ require (
 )
 
 require (
+	github.com/Azure/azure-sdk-for-go/sdk/azcore v1.13.0 // indirect
+	github.com/Azure/azure-sdk-for-go/sdk/azidentity v1.7.0 // indirect
+	github.com/Azure/azure-sdk-for-go/sdk/internal v1.10.0 // indirect
+	github.com/AzureAD/microsoft-authentication-library-for-go v1.2.2 // indirect
 	github.com/Knetic/govaluate v3.0.1-0.20171022003610-9aa49832a739+incompatible // indirect
+	github.com/alecthomas/units v0.0.0-20240626203959-61d1e3462e30 // indirect
+	github.com/aws/aws-sdk-go v1.54.19 // indirect
 	github.com/beorn7/perks v1.0.1 // indirect
 	github.com/bytedance/sonic v1.11.6 // indirect
 	github.com/bytedance/sonic/loader v0.1.1 // indirect
-	github.com/cespare/xxhash/v2 v2.2.0 // indirect
+	github.com/cespare/xxhash/v2 v2.3.0 // indirect
 	github.com/cloudwego/base64x v0.1.4 // indirect
 	github.com/cloudwego/iasm v0.2.0 // indirect
 	github.com/davecgh/go-spew v1.1.2-0.20180830191138-d8f796af33cc // indirect
@@ -53,7 +61,7 @@ require (
 	github.com/glebarez/go-sqlite v1.20.3 // indirect
 	github.com/glebarez/sqlite v1.7.0 // indirect
 	github.com/go-kit/log v0.2.1 // indirect
-	github.com/go-logfmt/logfmt v0.5.1 // indirect
+	github.com/go-logfmt/logfmt v0.6.0 // indirect
 	github.com/go-logr/logr v1.4.2 // indirect
 	github.com/go-openapi/jsonpointer v0.20.2 // indirect
 	github.com/go-openapi/jsonreference v0.20.4 // indirect
@@ -66,16 +74,20 @@ require (
 	github.com/google/gnostic-models v0.6.8 // indirect
 	github.com/google/go-cmp v0.6.0 // indirect
 	github.com/google/gofuzz v1.2.0 // indirect
+	github.com/grafana/regexp v0.0.0-20240518133315-a468a5bfb3bc // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
-	github.com/imdario/mergo v0.3.12 // indirect
+	github.com/imdario/mergo v0.3.16 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
 	github.com/jackc/pgx/v5 v5.4.3 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
+	github.com/jmespath/go-jmespath v0.4.0 // indirect
 	github.com/josharian/intern v1.0.0 // indirect
+	github.com/jpillora/backoff v1.0.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/cpuid/v2 v2.2.7 // indirect
+	github.com/kylelemons/godebug v1.1.0 // indirect
 	github.com/leodido/go-urn v1.4.0 // indirect
 	github.com/magiconair/properties v1.8.7 // indirect
 	github.com/mailru/easyjson v0.7.7 // indirect
@@ -85,15 +97,17 @@ require (
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/munnerz/goautoneg v0.0.0-20191010083416-a7dc8b61c822 // indirect
+	github.com/mwitkow/go-conntrack v0.0.0-20190716064945-2f068394615f // indirect
 	github.com/pelletier/go-toml/v2 v2.2.2 // indirect
-	github.com/prometheus/client_golang v1.18.0 // indirect
-	github.com/prometheus/client_model v0.5.0 // indirect
-	github.com/prometheus/common v0.46.0 // indirect
-	github.com/prometheus/procfs v0.12.0 // indirect
+	github.com/pkg/browser v0.0.0-20240102092130-5ac0b6a4141c // indirect
+	github.com/prometheus/client_golang v1.19.1 // indirect
+	github.com/prometheus/client_model v0.6.1 // indirect
+	github.com/prometheus/common/sigv4 v0.1.0 // indirect
+	github.com/prometheus/procfs v0.15.1 // indirect
 	github.com/remyoudompheng/bigfft v0.0.0-20230126093431-47fa9a501578 // indirect
 	github.com/sagikazarmark/locafero v0.4.0 // indirect
 	github.com/sagikazarmark/slog-shim v0.1.0 // indirect
-	github.com/shurcooL/httpfs v0.0.0-20190707220628-8d4bc4ba7749 // indirect
+	github.com/shurcooL/httpfs v0.0.0-20230704072500-f1e31cf0ba5c // indirect
 	github.com/shurcooL/vfsgen v0.0.0-20200824052919-0d455de96546 // indirect
 	github.com/sourcegraph/conc v0.3.0 // indirect
 	github.com/spf13/afero v1.11.0 // indirect
@@ -107,7 +121,7 @@ require (
 	github.com/x448/float16 v0.8.4 // indirect
 	go.uber.org/multierr v1.11.0 // indirect
 	golang.org/x/arch v0.8.0 // indirect
-	golang.org/x/exp v0.0.0-20230905200255-921286631fa9 // indirect
+	golang.org/x/exp v0.0.0-20240119083558-1b970713d09a // indirect
 	golang.org/x/net v0.29.0 // indirect
 	golang.org/x/oauth2 v0.21.0 // indirect
 	golang.org/x/sys v0.25.0 // indirect
--- internal/model/prometheus.go
+++ internal/model/prometheus.go
@@ -184,7 +184,7 @@ type MonitorScrapeJob struct {
 	Name                     string     `json:"name" binding:"required,min=1,max=50" gorm:"uniqueIndex;size:100;comment:采集任务名称，支持使用通配符*进行模糊搜索"` // 采集任务名称，支持使用通配符*进行模糊搜索
 	UserID                   int        `json:"userId" gorm:"comment:任务关联的用户ID"`                                                                // 任务关联的用户ID
 	Enable                   int        `json:"enable" gorm:"type:int;comment:是否启用采集任务：1为启用，2为禁用"`                                              // 是否启用采集任务：1为启用，2为禁用
-	ServiceDiscoveryType     string     `json:"serviceDiscoveryType,omitempty" gorm:"size:50;comment:服务发现类型，支持 k8s 或 tree-http"`                // 服务发现类型，支持 "k8s" 或 "tree-http"
+	ServiceDiscoveryType     string     `json:"serviceDiscoveryType,omitempty" gorm:"size:50;comment:服务发现类型，支持 k8s 或 http"`                     // 服务发现类型，支持 "k8s" 或 "http"
 	MetricsPath              string     `json:"metricsPath,omitempty" gorm:"size:255;comment:监控采集的路径"`                                          // 监控采集的路径
 	Scheme                   string     `json:"scheme,omitempty" gorm:"size:10;comment:监控采集的协议方案（如 http 或 https）"`                              // 监控采集的协议方案（如 http 或 https）
 	ScrapeInterval           int        `json:"scrapeInterval,omitempty" gorm:"default:30;type:int;comment:采集的时间间隔（秒）"`                         // 采集的时间间隔（秒）
@@ -211,20 +211,21 @@ type MonitorScrapeJob struct {
 // MonitorScrapePool 采集池的配置
 type MonitorScrapePool struct {
 	Model
-	Name                 string     `json:"name" binding:"required,min=1,max=50" gorm:"uniqueIndex;size:100;comment:采集池名称，支持使用通配符*进行模糊搜索"`        // 采集池名称，支持使用通配符*进行模糊搜索
-	PrometheusInstances  StringList `json:"prometheusInstances,omitempty" gorm:"type:text;comment:选择多个Prometheus实例"`                              // 选择多个Prometheus实例
-	UserID               int        `json:"userId" gorm:"comment:创建该采集池的用户ID"`                                                                    // 创建该采集池的用户ID
-	ScrapeInterval       int        `json:"scrapeInterval,omitempty" gorm:"default:30;type:int;comment:采集间隔（秒）"`                                  // 采集间隔（秒）
-	ScrapeTimeout        int        `json:"scrapeTimeout,omitempty" gorm:"default:10;type:int;comment:采集超时时间（秒）"`                                 // 采集超时时间（秒）
-	ExternalLabels       StringList `json:"externalLabels,omitempty" gorm:"type:text;comment:remote_write时添加的标签组，格式为 key=v，例如 scrape_ip=1.1.1.1"` // remote_write时添加的标签组，格式为 key=v，例如 scrape_ip=1.1.1.1
-	SupportAlert         int        `json:"supportAlert" gorm:"type:int;comment:是否支持告警：1支持，2不支持"`                                                 // 是否支持告警：1支持，2不支持
-	SupportRecord        int        `json:"supportRecord" gorm:"type:int;comment:是否支持预聚合：1支持，2不支持"`                                               // 是否支持预聚合：1支持，2不支持
-	RemoteReadUrl        string     `json:"remoteReadUrl,omitempty" gorm:"size:255;comment:远程读取的地址"`                                              // 远程读取的地址
-	AlertManagerUrl      string     `json:"alertManagerUrl,omitempty" gorm:"size:255;comment:AlertManager的地址"`                                    // AlertManager的地址
-	RuleFilePath         string     `json:"ruleFilePath,omitempty" gorm:"size:255;comment:规则文件路径"`                                                // 规则文件路径
-	RecordFilePath       string     `json:"recordFilePath,omitempty" gorm:"size:255;comment:记录文件路径"`                                              // 记录文件路径
-	RemoteWriteUrl       string     `json:"remoteWriteUrl,omitempty" gorm:"size:255;comment:远程写入的地址"`                                             // 远程写入的地址
-	RemoteTimeoutSeconds int        `json:"remoteTimeoutSeconds,omitempty" gorm:"default:5;type:int;comment:远程写入的超时时间（秒）"`                        // 远程写入的超时时间（秒）
+	Name                  string     `json:"name" binding:"required,min=1,max=50" gorm:"uniqueIndex;size:100;comment:采集池名称，支持使用通配符*进行模糊搜索"`        // 采集池名称，支持使用通配符*进行模糊搜索
+	PrometheusInstances   StringList `json:"prometheusInstances,omitempty" gorm:"type:text;comment:选择多个Prometheus实例"`                              // 选择多个Prometheus实例
+	AlertManagerInstances StringList `json:"alertManagerInstances,omitempty" gorm:"type:text;comment:选择多个AlertManager实例"`                          // 选择多个AlertManager实例
+	UserID                int        `json:"userId" gorm:"comment:创建该采集池的用户ID"`                                                                    // 创建该采集池的用户ID
+	ScrapeInterval        int        `json:"scrapeInterval,omitempty" gorm:"default:30;type:int;comment:采集间隔（秒）"`                                  // 采集间隔（秒）
+	ScrapeTimeout         int        `json:"scrapeTimeout,omitempty" gorm:"default:10;type:int;comment:采集超时时间（秒）"`                                 // 采集超时时间（秒）
+	ExternalLabels        StringList `json:"externalLabels,omitempty" gorm:"type:text;comment:remote_write时添加的标签组，格式为 key=v，例如 scrape_ip=1.1.1.1"` // remote_write时添加的标签组，格式为 key=v，例如 scrape_ip=1.1.1.1
+	SupportAlert          int        `json:"supportAlert" gorm:"type:int;comment:是否支持告警：1支持，2不支持"`                                                 // 是否支持告警：1支持，2不支持
+	SupportRecord         int        `json:"supportRecord" gorm:"type:int;comment:是否支持预聚合：1支持，2不支持"`                                               // 是否支持预聚合：1支持，2不支持
+	RemoteReadUrl         string     `json:"remoteReadUrl,omitempty" gorm:"size:255;comment:远程读取的地址"`                                              // 远程读取的地址
+	AlertManagerUrl       string     `json:"alertManagerUrl,omitempty" gorm:"size:255;comment:AlertManager的地址"`                                    // AlertManager的地址
+	RuleFilePath          string     `json:"ruleFilePath,omitempty" gorm:"size:255;comment:规则文件路径"`                                                // 规则文件路径
+	RecordFilePath        string     `json:"recordFilePath,omitempty" gorm:"size:255;comment:记录文件路径"`                                              // 记录文件路径
+	RemoteWriteUrl        string     `json:"remoteWriteUrl,omitempty" gorm:"size:255;comment:远程写入的地址"`                                             // 远程写入的地址
+	RemoteTimeoutSeconds  int        `json:"remoteTimeoutSeconds,omitempty" gorm:"default:5;type:int;comment:远程写入的超时时间（秒）"`                        // 远程写入的超时时间（秒）
 
 	// 前端使用字段
 	ExternalLabelsFront string `json:"externalLabelsFront,omitempty" gorm:"-"` // 前端显示的ExternalLabels字符串
