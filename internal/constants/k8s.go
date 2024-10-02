ff04f02bdfbc444b3844107ed25e26c3ec6868e9
shixiaocaia <shixiaocaia@gmail.com>
2024-10-02 16:21:03 +0800

    getPodsListByNodeName

--- internal/constants/k8s.go
+++ internal/constants/k8s.go
@@ -5,4 +5,6 @@ import "errors"
 var (
 	ErrorK8sClientNotReady     = errors.New("k8s client not ready")
 	ErrorMetricsClientNotReady = errors.New("metrics client not ready")
+
+	ErrorNodeNotFound = errors.New("node not found")
 )
