f97970e92cc5be944c8f3eb398fdc9a89b2d8018
Bamboo <13664854532@163.com>
2024-10-21 18:21:25 +0800

    完成服务树页面示例

--- web/apps/web-antd/src/router/routes/modules/servicetree.ts
+++ web/apps/web-antd/src/router/routes/modules/servicetree.ts
@@ -17,9 +17,27 @@ const routes: RouteRecordRaw[] = [
         name: '服务树概览',
         path: '/tree_overview',
         component: () =>
-          import('#/views/servicetree/tree_overview/TreeOverview.vue'),
+          import('#/views/servicetree/TreeOverview.vue'),
         meta: {
-          title: $t('page.serviceTree.overview'),
+          title: "服务树概览",
+        },
+      },
+      {
+        name: '服务树节点管理',
+        path: '/tree_node_manager',
+        component: () =>
+          import('#/views/servicetree/TreeNodeManager.vue'),
+        meta: {
+          title: "服务树节点管理",
+        },
+      },
+      {
+        name: '资产管理',
+        path: '/resource_operation',
+        component: () =>
+          import('#/views/servicetree/ResourceOperation.vue'),
+        meta: {
+          title: "资产管理",
         },
       },
     ],
