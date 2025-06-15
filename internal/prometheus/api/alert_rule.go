e974c010fba86f98bd66d6e9a7f4ac847c172dfd
Bamboo <13664854532@163.com>
2025-06-15 13:33:01 +0800

    fix: update

--- internal/prometheus/api/alert_rule.go
+++ internal/prometheus/api/alert_rule.go
@@ -26,8 +26,6 @@
 package api
 
 import (
-	"strconv"
-
 	ijwt "github.com/GoSimplicity/AI-CloudOps/pkg/utils"
 
 	"github.com/GoSimplicity/AI-CloudOps/internal/model"
@@ -57,11 +55,11 @@ func (a *AlertRuleHandler) RegisterRouters(server *gin.Engine) {
 		alertRules.GET("/list", a.GetMonitorAlertRuleList)
 		alertRules.POST("/promql_check", a.PromqlExprCheck)
 		alertRules.POST("/create", a.CreateMonitorAlertRule)
-		alertRules.POST("/update", a.UpdateMonitorAlertRule)
-		alertRules.POST("/enable", a.EnableSwitchMonitorAlertRule)
+		alertRules.POST("/update/:id", a.UpdateMonitorAlertRule)
+		alertRules.POST("/enable/:id", a.EnableSwitchMonitorAlertRule)
 		alertRules.POST("/batch_enable", a.BatchEnableSwitchMonitorAlertRule)
-		alertRules.DELETE("/:id", a.DeleteMonitorAlertRule)
-		alertRules.DELETE("/", a.BatchDeleteMonitorAlertRule)
+		alertRules.DELETE("/delete/:id", a.DeleteMonitorAlertRule)
+		alertRules.DELETE("/batch_delete", a.BatchDeleteMonitorAlertRule)
 		alertRules.GET("/total", a.GetMonitorAlertRuleTotal)
 	}
 }
@@ -71,124 +69,89 @@ func (a *AlertRuleHandler) CreateMonitorAlertRule(ctx *gin.Context) {
 	var alertRule model.MonitorAlertRule
 
 	uc := ctx.MustGet("user").(ijwt.UserClaims)
-
-	if err := ctx.ShouldBind(&alertRule); err != nil {
-		utils.ErrorWithDetails(ctx, err, "参数错误")
-		return
-	}
-
 	alertRule.UserID = uc.Uid
 
-	if err := a.alertRuleService.CreateMonitorAlertRule(ctx, &alertRule); err != nil {
-		utils.ErrorWithMessage(ctx, err.Error())
-		return
-	}
-
-	utils.Success(ctx)
+	utils.HandleRequest(ctx, &alertRule, func() (interface{}, error) {
+		return nil, a.alertRuleService.CreateMonitorAlertRule(ctx, &alertRule)
+	})
 }
 
 // UpdateMonitorAlertRule 更新现有的告警规则
 func (a *AlertRuleHandler) UpdateMonitorAlertRule(ctx *gin.Context) {
-	var alertRule model.MonitorAlertRule
+	var req model.MonitorAlertRule
 
-	if err := ctx.ShouldBind(&alertRule); err != nil {
-		utils.ErrorWithDetails(ctx, err, "参数错误")
-		return
-	}
-
-	if err := a.alertRuleService.UpdateMonitorAlertRule(ctx, &alertRule); err != nil {
+	id, err := utils.GetParamID(ctx)
+	if err != nil {
 		utils.ErrorWithMessage(ctx, err.Error())
 		return
 	}
 
-	utils.Success(ctx)
+	req.ID = id
+
+	utils.HandleRequest(ctx, &req, func() (interface{}, error) {
+		return nil, a.alertRuleService.UpdateMonitorAlertRule(ctx, &req)
+	})
 }
 
 // EnableSwitchMonitorAlertRule 切换告警规则的启用状态
 func (a *AlertRuleHandler) EnableSwitchMonitorAlertRule(ctx *gin.Context) {
 	var req model.IdRequest
 
-	if err := ctx.ShouldBind(&req); err != nil {
-		utils.ErrorWithDetails(ctx, err, "参数错误")
-		return
-	}
-
-	if err := a.alertRuleService.EnableSwitchMonitorAlertRule(ctx, req.ID); err != nil {
+	id, err := utils.GetParamID(ctx)
+	if err != nil {
 		utils.ErrorWithMessage(ctx, err.Error())
 		return
 	}
 
-	utils.Success(ctx)
+	req.ID = id
+
+	utils.HandleRequest(ctx, &req, func() (interface{}, error) {
+		return nil, a.alertRuleService.EnableSwitchMonitorAlertRule(ctx, req.ID)
+	})
 }
 
 // BatchEnableSwitchMonitorAlertRule 批量切换告警规则的启用状态
 func (a *AlertRuleHandler) BatchEnableSwitchMonitorAlertRule(ctx *gin.Context) {
 	var req model.BatchRequest
 
-	if err := ctx.ShouldBindJSON(&req); err != nil {
-		utils.ErrorWithDetails(ctx, err, "参数错误")
-		return
-	}
-
-	if err := a.alertRuleService.BatchEnableSwitchMonitorAlertRule(ctx, req.IDs); err != nil {
-		utils.ErrorWithMessage(ctx, err.Error())
-		return
-	}
-
-	utils.Success(ctx)
+	utils.HandleRequest(ctx, &req, func() (interface{}, error) {
+		return nil, a.alertRuleService.BatchEnableSwitchMonitorAlertRule(ctx, req.IDs)
+	})
 }
 
 // DeleteMonitorAlertRule 删除指定的告警规则
 func (a *AlertRuleHandler) DeleteMonitorAlertRule(ctx *gin.Context) {
-	id := ctx.Param("id")
+	var req model.DeleteMonitorAlertRuleRequest
 
-	intId, err := strconv.Atoi(id)
+	id, err := utils.GetParamID(ctx)
 	if err != nil {
-		utils.ErrorWithMessage(ctx, "参数错误")
-		return
-	}
-
-	if err := a.alertRuleService.DeleteMonitorAlertRule(ctx, intId); err != nil {
 		utils.ErrorWithMessage(ctx, err.Error())
 		return
 	}
 
-	utils.Success(ctx)
+	req.ID = id
+
+	utils.HandleRequest(ctx, &req, func() (interface{}, error) {
+		return nil, a.alertRuleService.DeleteMonitorAlertRule(ctx, req.ID)
+	})
 }
 
 // BatchDeleteMonitorAlertRule 批量删除告警规则
 func (a *AlertRuleHandler) BatchDeleteMonitorAlertRule(ctx *gin.Context) {
 	var req model.BatchRequest
 
-	if err := ctx.ShouldBindJSON(&req); err != nil {
-		utils.ErrorWithDetails(ctx, err, "参数错误")
-		return
-	}
-
-	if err := a.alertRuleService.BatchDeleteMonitorAlertRule(ctx, req.IDs); err != nil {
-		utils.ErrorWithMessage(ctx, err.Error())
-		return
-	}
-
-	utils.Success(ctx)
+	utils.HandleRequest(ctx, &req, func() (interface{}, error) {
+		return nil, a.alertRuleService.BatchDeleteMonitorAlertRule(ctx, req.IDs)
+	})
 }
 
 // GetMonitorAlertRuleList 获取告警规则列表
 func (a *AlertRuleHandler) GetMonitorAlertRuleList(ctx *gin.Context) {
 	var listReq model.ListReq
 
-	if err := ctx.ShouldBindQuery(&listReq); err != nil {
-		utils.ErrorWithDetails(ctx, err, "参数错误")
-		return
-	}
-
-	list, err := a.alertRuleService.GetMonitorAlertRuleList(ctx, &listReq)
-	if err != nil {
-		utils.ErrorWithMessage(ctx, err.Error())
-		return
-	}
-
-	utils.SuccessWithData(ctx, list)
+	utils.HandleRequest(ctx, &listReq, func() (interface{}, error) {
+		return a.alertRuleService.GetMonitorAlertRuleList(ctx, &listReq)
+	})
 }
 
 // PromqlExprCheck 检查 PromQL 表达式的合法性
