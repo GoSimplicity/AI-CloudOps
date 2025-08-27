3d74f045fe559d47eba18583c8534910fd6eacff
Bamboo <13664854532@163.com>
2025-08-27 09:26:33 +0800

    fix: update user management and system audit models file name, update user profile endpoint

--- internal/constants/user.go
+++ internal/constants/user.go
@@ -35,16 +35,6 @@ const (
 )
 
 var (
-	// UserService
-	ErrorUserExist         = errors.New("user already exists, check your username or mobile, or try to login")
 	ErrorUserNotExist      = errors.New("user not exists")
-	ErrorUserSignUpFail    = errors.New("user sign up fail")
 	ErrorPasswordIncorrect = errors.New("user password incorrect")
-
-	// TreeService
-	// Node DAO
-	ErrorTreeNodeNotExist = errors.New("tree node not exists")
-
-	// ECS DAO
-	ErrorResourceEcsExist = errors.New("resource ecs already exists")
 )
